0955f847d9379e00e34b29cba9852447b347da5f
aagaathaa <agathaabakumova@gmail.com>
2026-06-10 15:37:44 +0300

    Allow config.py to be tracked by Git

---
+++ config.py
@@ -0,0 +1,18 @@
+# Locators
+USERNAME_FIELD = "#user-name"
+PASSWORD_FIELD = "#password"
+LOGIN_BUTTON = "#login-button"
+ERROR_MESSAGE = ".error-message-container"
+
+# Users data
+UNIQ_PASSWORD = "secret_sauce"
+STANDARD_USERNAME = "standard_user"
+WRONG_USERNAME = "standart_usr"
+LOCKED_OUT_USERNAME = "locked_out_user"
+
+# Buttons
+__ADD_TO_CART_BTN = ".btn_inventory "
+
+# Elements
+__INVENTORY_ITEM = ".inventory_item"
+__INVENTORY_ITEM_NAME = ".inventory_item_name"
